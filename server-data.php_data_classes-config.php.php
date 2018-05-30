5eea09cd9d44205b8f4c5e617f83eadf7ef61c75
Takesoul <38192328+Takesoul@users.noreply.github.com>
2018-05-30 17:19:24 -0300

    aadddd
    adsdadadadad

---
+++ server-data.php_data_classes-config.php.php
@@ -0,0 +1,29 @@
+<?php
+#####################################################################
+#||||| HobbaCMS - Sistema de administração de conteudo Habbo   |||||#
+#||||| Baseada em HabboCMS - HTML, PHP, CSS                    |||||#
+# ----------------------------------------------------------------- #
+#||||| © Copyright 2013 Hey 2013 - Open Source Software      |||||#
+#||||| © Copyright 2013 Habbo - Estrutura antiga 2011/2013     |||||#
+#||||| © Copyright 2013 Sulake Corporation - Habbo Arquivos    |||||#
+# ----------------------------------------------------------------- #
+#||||| HobbaCMS é uma estrutura se site baseado em Habbo Hotel |||||#
+#||||| Os arquivos são protegido por um único arquivo          |||||#
+#####################################################################
+
+################## PREFERÊNCIAS ##################
+$Hotelname = "Hirbo"; //*Nome do Hotel, exemplo = Habbo*//
+$hotel_url = "http://127.0.0.1"; //*URL do Hotel, sem o "www"*//
+
+################## CONFIGURAÇÕES ##################
+//Ajustes os dados de acordo com a senha do seu MySQL
+
+$MySQLhostname = "localhost"; 
+$MySQLusername = "root";
+$MySQLpassword = ""; //*Senha MySQL*//
+$MySQLdb = "habbo"; //*Banco de dados*//
+$hotel_url = "http://127.0.0.1"; //*URL do Hotel*//
+
+################## OUTRAS CONFIGURAÇÕES ###############
+$avatar = "http://www.habbo.fr/habbo-imaging/avatarimage?figure="; //*Avatar Hospedados*//
+$cimages_url = "http://swf.hostzh.net/images/c_images/"; //*Emblemas hospedados*//
